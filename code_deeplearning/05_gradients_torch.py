0ade3db06528407c3f360f4df493c374eeb59db7
Dahuia <dahuiaei@gmail.com>
2022-10-17 22:21:52 +0800

    10.17 until 08_logistic_regression

--- code_deeplearning/05_gradients_torch.py
+++ code_deeplearning/05_gradients_torch.py
@@ -1,33 +1,39 @@
+# 1 ) Design model (input, output size, forward pass)
+# 2 ) Construct loss and optimizer
+# 3 ) Training loop
+#       - forward pass: compute prediction
+#       - backward pass: gradients
+#       - update weights
+
 import torch
+import torch.nn as nn
 
 # f = w * x
 
 # f = 2 * x
-x = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+x = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+x_test = torch.tensor([5],dtype=torch.float32)
 
+n_samples, n_features = x.shape
+input_size = n_features
+output_size = n_features
 
-# model prediction
-def forward(x):
-    return w * x
+model = nn.Linear(input_size, output_size)
 
-
-# loss = MSE
-def loss(y, y_predicted):
-    return ((y_predicted - y) ** 2).mean()
-
-
-print(f'Prediction before training: f(5) = {forward(5):.3f}')
+print(f'Prediction before training: f(5) = {model(x_test).item():.3f}')
 
 # Training
 learning_rate = 0.01
 n_iters = 100
 
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
 for epoch in range(n_iters):
     # prediction = forward pass
-    y_pred = forward(x)
+    y_pred = model(x)
 
     # loss
     l = loss(y, y_pred)
@@ -36,11 +42,11 @@ for epoch in range(n_iters):
     l.backward()
 
     # update weights
-    with torch.no_grad():
-        w -= learning_rate * w.grad
+    optimizer.step()
 
-    w.grad.zero_()
+    optimizer.zero_grad()
     if epoch % 10 == 0:
-        print(f'epoch {epoch + 1}:w = {w:.3f}, loss = {l:.8f}')
+        [w, b] = model.parameters()
+        print(f'epoch {epoch + 1}:w = {w[0][0].item():.3f}, loss = {l:.8f}')
 
-print(f'Prediction after training: f(5) = {forward(5):.3f}')
+print(f'Prediction after training: f(5) = {model(x_test).item():.3f}')
